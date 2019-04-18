1bd009d5b736b6a33e5dbff5ee1f67f2da62ec4a
Harry Wigman <harrywigman@redweb.com>
2019-04-18 16:26:06 +0100

    Add project files.

---
+++ MediaOrganiser/Bootstapper.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using MediaOrganiser.ViewModels;
+
+namespace MediaOrganiser
+{
+    public class Bootstapper : BootstrapperBase
+    {
+        public Bootstapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MenuViewModel>();
+        }
+    }
+}
